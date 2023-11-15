ce7930cc8fbc6b040caaead635c344f735d0055e
Marko Sego <markosego@gmail.com>
2023-11-15 21:17:02 +0100

    Day4/5

--- Day4/day4.fsx
+++ Day4/day4.fsx
@@ -1,28 +1,67 @@
 #load "..\\InputHelper\\InputHelper.fsx"
 open InputHelper
 
+let mutableDict () = new System.Collections.Generic.Dictionary<string, string>()
 let input = InputHelper.input 4
 
 let getPassports (sequence: seq<string>) =
-    let passport = System.Collections.Generic.Dictionary<string, string>()
-    seq {for line in sequence do
-            if line = System.Environment.NewLine || line = "" then 
+    [let mutable passport = mutableDict ()
+     for line in sequence do
+            if line = "" then 
                 yield passport
-                passport.Clear()
+                passport <- mutableDict ()
             for part in line.Split(" ") do
-                let keyValue = part.Split(":")
-                passport.Add(keyValue[0], keyValue[1])}
+                if part <> "" then
+                    let keyValue = part.Split(":")
+                    passport.Add(keyValue[0], keyValue[1])]
+
+let yearValid lower upper (year: int) = year >= lower && year <= upper
+let byrValid = yearValid 1920 2002
+let iyrValid = yearValid 2010 2020
+let eyrValid = yearValid 2020 2030
+let hgtValid (hgt: string) =
+    let hgtCheck = System.Text.RegularExpressions.Regex("^[1-9]{1}[0-9]{1,2}(cm|in)$")
+    if hgtCheck.IsMatch hgt
+    then let value = int (hgt |> Seq.take (hgt.Length - 2) |> System.String.Concat)
+         let unit = hgt |> Seq.skip (hgt.Length - 2) |> System.String.Concat
+         if unit = "cm"
+         then value >= 150 && value <= 193
+         else value >= 59 && value <= 76
+    else false
+let hclValid (hcl: string) = 
+    let hclCheck = System.Text.RegularExpressions.Regex("^#[a-f0-9]{6}$")
+    hclCheck.IsMatch hcl
+let eclValid (ecl: string) =
+    let eclCheck = System.Text.RegularExpressions.Regex("^(amb|blu|brn|gry|grn|hzl|oth)$")
+    eclCheck.IsMatch ecl
+let pidValid (pid: string) =
+    let pidCheck = System.Text.RegularExpressions.Regex("^[0-9]{9}$")
+    pidCheck.IsMatch pid
+
+let passportValid (passport: System.Collections.Generic.Dictionary<string, string>) =
+    passport.ContainsKey "byr" && 
+    passport.ContainsKey "iyr" && 
+    passport.ContainsKey "eyr" && 
+    passport.ContainsKey "hgt" && 
+    passport.ContainsKey "hcl" && 
+    passport.ContainsKey "ecl" && 
+    passport.ContainsKey "pid"
+
+let passportReallyValid passport =
+    passportValid passport &&
+    byrValid (int passport["byr"]) &&
+    iyrValid (int passport["iyr"]) &&
+    eyrValid (int passport["eyr"]) &&
+    hgtValid (passport["hgt"]) &&
+    hclValid (passport["hcl"]) &&
+    eclValid (passport["ecl"]) &&
+    pidValid (passport["pid"])
+
 
 let passports = getPassports input
 
-let passportValid (passport: System.Collections.Generic.Dictionary<string, string>) =
-    passport.ContainsKey "byr" &&
-    passport.ContainsKey "iyr" &&
-    passport.ContainsKey "eyr" &&
-    passport.ContainsKey "hgt" &&
-    passport.ContainsKey "hcl" &&
-    passport.ContainsKey "ecl" &&
-    passport.ContainsKey "pid"
-
-let result1 = passports |> Seq.filter passportValid |> Seq.length
+let result1 = passports |> List.filter passportValid |> List.length
 printfn $"{result1}"
+
+let result2 = passports |> List.filter passportReallyValid |> List.length
+printfn $"{result2}"
---
+++ Day5/day5-improved.fsx
@@ -0,0 +1,82 @@
+#load "..\\InputHelper\\InputHelper.fsx"
+open InputHelper
+
+let input = InputHelper.input 5
+
+type Direction = Upper | Lower
+type Range = Range of (int * int) | Single of int
+
+let getUpper (range: (int * int)) = 
+    let (lower, upper) = range
+    let lowerBoundary = (lower + upper) / 2
+    if lowerBoundary + 1 = upper
+    then Single (upper)
+    else Range (lowerBoundary + 1, upper)
+let getLower (range: (int * int)) = 
+    let (lower, upper) = range
+    let lowerBoundary = (lower + upper) / 2
+    if lower = lowerBoundary
+    then Single (lower)
+    else Range (lower, lowerBoundary)
+
+let rec getSeatValue (input: Direction list) (start: Range) : int =
+    match start with
+    | Single point -> point
+    | Range (lower, upper) ->
+        match input with
+        | head :: tail ->
+            match head with
+            | Upper -> getSeatValue tail (getUpper (lower, upper))
+            | Lower -> getSeatValue tail (getLower (lower, upper))
+        | [] -> failwith "data error"
+
+let getSeatId (input: Direction list) : int =
+    let rowId = getSeatValue (input |> List.take 7) (Range (0, 127))
+    let columnId = getSeatValue (input |> List.skip 7) (Range (0, 7))
+    rowId * 8 + columnId
+
+let mapInputLine (line: string) : Direction list =
+    [for c in line do
+        match c with
+        | 'F' -> yield Lower
+        | 'B' -> yield Upper
+        | 'L' -> yield Lower
+        | 'R' -> yield Upper
+        | _ -> failwith "input error"]
+
+let result1 = 
+    input 
+    |> Seq.map (fun line -> line |> mapInputLine |> getSeatId) 
+    |> Seq.max
+printfn "%A" result1
+
+let getRemainingPositions (positions: int Set) =
+    [for column in 0..7 do
+        for row in 0..127 do
+            let position = row * 8 + column
+            if (positions |> Set.contains position) = false
+            then yield position]
+
+let filterPositionNotOnTheEdge (seatId: int) =
+    seatId / 8 <> 0 && seatId / 8 <> 127
+
+let getSolution (seatIds: int list) : int =
+    let solution = 
+        seatIds 
+        |> List.pairwise 
+        |> List.pick (
+            fun (first, second) -> 
+            if System.Math.Abs(first - second) > 2 
+            then Some first 
+            else None)
+    solution
+
+let result2 = 
+    input 
+    |> Seq.map (fun line -> line |> mapInputLine |> getSeatId)
+    |> set 
+    |> getRemainingPositions
+    |> List.filter filterPositionNotOnTheEdge
+    |> List.sort
+    |> getSolution
+printfn "%A" result2
